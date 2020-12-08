81099674cadac9bdf78f26d93300249416cbe257
Jonathan Frederick <jonathanf@uriux.com>
2020-12-08 14:10:08 -0800

    Change how SQL connection works

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ const sql_pass = process.env.DB_PASS || 'acme';
 const sql_db = process.env.DB_NAME || 'testdb';
 const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
 
-if (process.env.DB_HOST) {
+if (process.env.CLOUD_SQL_CONNECTION_NAME) {
+  console.log(`Establishing SQL connection to ${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`);
+  var sql_connection_config = {
+    socketPath : `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+    user     : sql_user,
+    password : sql_pass,
+    database : sql_db
+  }
+} else {
   const sql_tmp = process.env.DB_HOST || '127.0.0.1:3306';
   const sql_host = sql_tmp.split(':')[0];
   const sql_port = sql_tmp.split(':')[1];
@@ -25,33 +33,7 @@ if (process.env.DB_HOST) {
     password : sql_pass,
     database : sql_db
   }
-} else if (process.env.CLOUD_SQL_CONNECTION_NAME) {
-  console.log(`Establishing SQL connection to ${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`);
-  var sql_connection_config = {
-    socketPath : `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
-    user     : sql_user,
-    password : sql_pass,
-    database : sql_db
-  }
-} else {
-  console.error("Neither DB_HOST nor CLOUD_SQL_CONNECTION_NAME was defined, bailing out");
-  process.exit(-1);
 }
-
-var sql_connection = mysql.createConnection(sql_connection_config);
-
-sql_connection.connect(function(err) {
-  if (err) {
-    console.error('SQL error connecting: ' + err.stack);
-    throw err;
-  }
-
-  console.log('SQL connected as id ' + sql_connection.threadId);
-});
-
-process.on('exit', function() {
-  sql_connection.end();
-});
 
 // Use handlebars for the view engine, for easy templating
 app.engine('handlebars', handlebars());
@@ -64,11 +46,32 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   console.log(`Got Request: ${req.method} ${req.originalUrl}`);
   console.log(`  Getting userlist from SQL...`);
-  sql_connection.query('SELECT * FROM userdb.users;', (err, userlist, fields) =>{
-    if (err) throw err;
-    console.log(`  Got userlist from SQL successfully!`);
-    res.render('home', {users: userlist});
+  var sql_connection = mysql.createConnection(sql_connection_config);
+  sql_connection.connect(function(err) {
+    if (err) {
+      const rettxt = `  SQL error connecting: ${err.stack}`;
+      console.error(rettxt);
+      res.send(rettxt);
+      sql_connection.end();
+    } else {
+
+      console.log('  SQL connected as id ' + sql_connection.threadId);
+
+      sql_connection.query('SELECT username,user_id,user_creation_date FROM userdb.users;', (err, userlist, fields) => {
+        if (err) {
+          const rettxt = `SQL error during query: ${err.stack}`;
+          console.error(rettxt);
+          res.send(rettxt);
+          sql_connection.end();
+        } else {
+          console.log(`  Got userlist from SQL successfully!`);
+          res.send(JSON.stringify(userlist));
+          sql_connection.end();
+        }
+      });
+    }
   });
+
 })
 
 app.listen(port, () => {
